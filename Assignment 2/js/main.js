1578a90fa47af3fe2b9ae30b7954378d299fe13d
desh-woes <oadesina17@alustudent.com>
2019-03-31 19:36:41 +0200

    Assignment 2 -- Fixed bugs with quiz 1.

--- Assignment 2/js/main.js
+++ Assignment 2/js/main.js
@@ -2,6 +2,10 @@
 let answers_quiz1  = ["a", "b", "c", "d", "a", "b"];
 let question_arr_Q1 = ["question1", "question2", "question3", "question4", "question5", "question6"];
 
+// List containing the id's of the images to be displayed when a question is answered.
+let mark_img = ["img1", "img2", "img3", "img4", "img5", "img6"];
+
+// Function to check responses in the first quiz
 function check_response() {
     //Count to keep track of the number of correct choices
     let count = 0;
@@ -14,6 +18,9 @@ function check_response() {
         // Variable to get the checked box in the current value
         let curr_question_val = document.querySelector('input[name="'+curr_question+'"]:checked');
 
+        // Question mark img
+        let Q_img = document.getElementById(mark_img[i]);
+
         // logic to make sure all questions are checked before submission
         if (curr_question_val == null){
             alert("Fucking😒😒 answer all questions Idiot");
@@ -23,9 +30,12 @@ function check_response() {
         // Increase count if all questions have been checked and the checked value is correct
         else if (curr_question_val.value === answers_quiz1[i]){
             count++;
+            Q_img.src = "Images/tick.png";
         }
-
+        // Make the images visible
+        Q_img.style.display = "block";
     }
+
 
     // After all questions have been answered, display the result box alongside the appropriate message
     let result_tab = document.querySelector(".result");
